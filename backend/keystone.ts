ee6da8afb2a95a0303d3495b5bca14fac0c755d0
Jelena <47182797+JDugonjic@users.noreply.github.com>
2021-03-31 14:22:57 +0200

    User schema added

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,2 +1,60 @@
-// eslint-disable-next-line prettier/prettier
-import "dotenv/config";
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+
+const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+
+    // TODO: Add initial roles here
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: Add data seeding here
+    },
+
+    lists: createSchema({
+      // Schema items go in here
+      User,
+    }),
+    ui: {
+      // TODO: Change this for roles
+      isAccessAllowed: ({ session }) => {
+        console.log(session);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        return !!session?.data;
+      },
+    },
+
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id',
+    }),
+  })
+);
