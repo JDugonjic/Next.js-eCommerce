8cb099a9264c344b19ab338fc6275512f4e28e09
Jelena <47182797+JDugonjic@users.noreply.github.com>
2021-04-29 14:31:27 +0200

    Update product mutation and UpdateProduct component added

--- components/SingleProduct.js
+++ components/SingleProduct.js
@@ -1,5 +1,7 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
+import Head from 'next/head';
+import styled from 'styled-components';
 import DisplayError from './DisplayError';
 
 const SINGLE_ITEM_QUERY = gql`
@@ -33,16 +35,33 @@ export default function SingleProduct({ id }) {
   }
 
   return (
-    <div>
+    <ProductStyles>
+      <Head>
+        <title>Sick Fits | {data.Product.name}</title>
+      </Head>
+      <img
+        src={data.Product.photo.image.publicUrlTransformed}
+        alt={data.Product.photo.altText}
+      />
       <div className="details">
-        <img
-          src={data.Product.photo.image.publicUrlTransformed}
-          alt={data.Product.photo.altText}
-        />
         <h2>The {data.Product.name}</h2>
         <p>{data.Product.description}</p>
         <p>{data.Product.price}</p>
       </div>
-    </div>
+    </ProductStyles>
   );
 }
+
+const ProductStyles = styled.div`
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  max-width: var(--maxWidth);
+  justify-content: center;
+  align-items: top;
+  gap: 2rem;
+  img {
+    width: 100%;
+    object-fit: contain;
+  }
+`;
